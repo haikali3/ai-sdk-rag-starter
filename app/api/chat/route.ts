30198110e635451726b77c89d590331f6c19b9b9
haikali3 <manfdvcl9@gmail.com>
2025-06-06 22:03:09 +0800

    Enhance chat functionality and resource creation by integrating AI embeddings. Added chat component using @ai-sdk/react and updated resource creation to generate embeddings upon resource creation.

---
+++ app/api/chat/route.ts
@@ -0,0 +1,39 @@
+import { createResource } from '@/lib/actions/resources';
+import { openai } from '@ai-sdk/openai';
+import { streamText, tool } from 'ai';
+import { z } from 'zod';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."
+    You have access to the following tools:
+    - search: search the knowledge base for information
+    - createResource: create a new resource
+    - updateResource: update an existing resource
+    - deleteResource: delete an existing resource
+    `,
+    messages,
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        parameters: z.object({
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
+  });
+
+  return result.toDataStreamResponse();
+}
